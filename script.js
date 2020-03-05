7692440ff115e6c12faa474d7fb5932b11cf3363
shdonas <BlackHole7473>
2020-03-05 10:40:22 -0500

    Added css and currency api

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ var appid = "f4cc075ea301646a421c78dc383a795a"
 
 // http://api." + apiProvider +"/data/2.5/weather?q=" + city + "&units=" + units + "&APPID=" + appid
 
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?zip=19446&units=metric&appid=f4cc075ea301646a421c78dc383a795a", 
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?zip=19446&units=imperial&appid=f4cc075ea301646a421c78dc383a795a", 
 function(data){
     console.log(data);
 
     var location = data.name;
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var weather = data.weather[0].main;
-    var temp = data.main.temp + "C";
+    var temp = data.main.temp + "F";
     var feelsLike = "Feels Like: " + data.main.feels_like;
 
     var lon = "Longitude: " + data.coord.lon;
@@ -30,4 +30,13 @@ function(data){
 
     $(".lon").append(lon);
     $(".lat").append(lat);
+})
+
+$.getJSON("http://data.fixer.io/api/latest?access_key=c136a6fbf1861caf57879b4cddaed923",
+function(currencyData){
+    console.log(currencyData);
+
+    var euroToUsd = 1/currencyData.rates.USD
+    var rate = "One USD = " + parseFloat(euroToUsd).toFixed(2) + " EURO";
+    $('.rate').append(rate);
 })
